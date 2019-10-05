f582e0f274f8f3615d9c00d63a36713a805d2a1c
Odole Kayode <kayode@patricia.com.ng>
2019-10-05 19:04:29 +0100

    V1 of the Goodthoughts Generator

---
+++ README.md
@@ -0,0 +1,5 @@
+# This package was designed to assist developers.
+
+# A good thought is generated each time the static method generateThought() is called.
+
+
---
+++ src/Application/Goodthoughts.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Application;
+
+class Goodthoughts {
+
+	/**
+	 * This static method generates a "Good Thought" from an array of "Good Thought"
+	 * 
+	 * @return String
+	 */
+	public static function generateThought()
+	{
+		$thoughtArray = [
+			"You are a great programmer",
+			"Relax, try again later mate !",
+			"Don't stop till you finish !",
+			"Tomorrow is another day !",
+			"Your codes will compile now ,Trust me !,",
+			"You can do this !",
+			"Yeah, You did it ! Congrats !"
+		];
+
+		$index = mt_rand(0,6);
+
+		return $thoughtArray[$index];
+	}
+
+}
